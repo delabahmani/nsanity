0a26e7a7754be69c17c7c1beea95559b67a3a30a
Dela Bahmani <delabahmani@gmail.com>
2025-10-08 13:13:47 -0400

    fix: removed redundant admin page and replaced with manage products. wrapped btns in link

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -12,21 +12,29 @@ export default async function AdminPage() {
   }
 
   return (
-    <div className="min-h-screen nav-pad flex flex-col">
-      <div className="flex p-10 items-center justify-center">
-        <h1 className="text-black text-4xl font-semibold">admin page</h1>
+    <div className="flex min-h-screen nav-pad flex-col">
+      <div className="flex py-10 items-center justify-center">
+        <h1 className="text-black font-bold text-3xl text-center">
+          manage products
+        </h1>
       </div>
 
       <div className="grid lg:grid-cols-3 max-lg:grid-rows-3 gap-4 py-10 w-[50%] mx-auto">
-        <Button variant="primary" className="text-md md:text-xl h-14">
-          <Link href={"/admin/products"}>manage products</Link>
-        </Button>
-        <Button variant="primary" className="text-md md:text-xl h-14">
-          user management
-        </Button>
-        <Button variant="primary" className="text-md md:text-xl h-14">
-          settings
-        </Button>
+        <Link href={"/admin/products/addProduct"}>
+          <Button variant="primary" className="text-md md:text-xl h-14 w-full">
+            add product
+          </Button>
+        </Link>
+        <Link href={"/admin/products/editProduct"}>
+          <Button variant="primary" className="text-md md:text-xl h-14 w-full">
+            edit product
+          </Button>
+        </Link>
+        <Link href={"/admin/products/deleteProduct"}>
+          <Button variant="primary" className="text-md md:text-xl h-14 w-full">
+            delete product
+          </Button>
+        </Link>
       </div>
     </div>
   );
